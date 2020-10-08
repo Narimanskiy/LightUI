ba289e20321cfc458d2e317b9ab5aa91574cfd36
Nariman Narimanov <Narimanov7@yandex.ru>
2020-10-08 17:58:36 +0300

    Initial Commit

---
+++ LightUI/LightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightUIApp.swift
+//  LightUI
+//
+//  Created by Нариман on 08.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct LightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
